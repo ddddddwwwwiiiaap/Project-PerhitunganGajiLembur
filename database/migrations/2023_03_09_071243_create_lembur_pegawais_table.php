e4553ff11f6311c8b7a4badfb73836decdf8ca9b
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2023-03-09 14:42:20 +0700

    menambahkan atribut lembur pegawai

--- database/migrations/2023_03_09_071243_create_lembur_pegawais_table.php
+++ database/migrations/2023_03_09_071243_create_lembur_pegawais_table.php
@@ -14,7 +14,14 @@ class CreateLemburPegawaisTable extends Migration
     public function up()
     {
         Schema::create('lembur_pegawais', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('kategori_lembur_id')->unsigned();
+            $table->foreign('kategori_lembur_id')->on('kategori_lemburs')->references('id')->onDelete('Cascade')->onUpdate('Cascade');
+            $table->integer('staff_id')->unsigned();
+            $table->foreign('staff_id')->on('tb_staff')->references('id')->onDelete('Cascade')->onUpdate('Cascade');
+            $table->time('mulai_lembur');
+            $table->time('selesai_lembur');
+            $table->integer('jumlah_jam');
             $table->timestamps();
         });
     }
