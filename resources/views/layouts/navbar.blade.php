79c305c0efa87ecbeeb74809b876f53f3b34adbb
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2023-03-27 10:54:25 +0700

    memperbaiki warna

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background-color: #014a94 !important;">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ url('/') }}" style="color: #fff">
             @guest
             Perhitungan Gaji Lembur
             @else
